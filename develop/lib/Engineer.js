1dbdcdac87cadbc283508b4ab8f0f7138bf14584
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-29 23:24:01 -0400

    added manager class

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,7 +1,7 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 class Engineer extends Employee {
   constructor(name, id, email, github, getName, getId, getEmail, getRole, getGithub, getRole) {
-    super (name, id, email);
+    super (name, id, email, getName, getEmail, getId);
     this.github = github;
   }
 
--- develop/lib/Intern.js
+++ develop/lib/Intern.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern extends Employee {
+  constructor(name, id, email, school, getName, getId, getEmail, getSchool, getRole) {
+    super(name, id, email, getName, getId, getEmail);
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    return 'Intern';
+  }
+}
